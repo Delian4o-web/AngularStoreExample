db3e54d62491d955ebc57abcf2149dbe124efc1a
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-17 13:16:26 +0200

    feat : Added top bar component

--- store-example/src/app/app.module.ts
+++ store-example/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
 
 @NgModule({
   declarations: [
@@ -14,11 +14,8 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     ProductAlertsComponent,
     TopBarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
