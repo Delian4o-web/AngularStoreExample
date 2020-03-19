088865f06c4acdd3798a9a24e628819f45cff153
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-19 09:52:15 +0200

    Add Routing

--- store-example/src/app/app.module.ts
+++ store-example/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
-import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    TopBarComponent,
     ProductDetailsComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: "", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
